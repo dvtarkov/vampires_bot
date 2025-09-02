b3dc617b896e35e5f3cd7a79cc97015fa51a879c
Not That Droids <dv.tarkov@google.com>
2025-09-02 19:37:21 +0200

    exports folder added

--- services/notify.py
+++ services/notify.py
@@ -2,6 +2,7 @@
 from typing import Optional
 from aiogram import Bot
 from screens.notify_screen import NotifyScreen
+import logging
 
 
 async def notify_user(
@@ -17,16 +18,19 @@ async def notify_user(
     Отправляет пользователю push‑уведомление (в его личку с ботом).
     Требует: BaseScreen._render умеет работать с chat_id и bot.
     """
-    screen = NotifyScreen()
-    await screen.run(
-        message=None,            # можно без исходного message
-        bot=bot,                 # <- обязательно передаём bot
-        chat_id=user_tg_id,      # <- явный чат для доставки
-        title=title,
-        body=body,
-        parse_mode=parse_mode,
-        render_kind="notice",    # на всякий случай дублируем
-        force_new=True,
-        persist_key=persist_key or f"notify:{user_tg_id}",
-        disable_web_page_preview=True,
-    )
+    try:
+        screen = NotifyScreen()
+        await screen.run(
+            message=None,            # можно без исходного message
+            bot=bot,                 # <- обязательно передаём bot
+            chat_id=user_tg_id,      # <- явный чат для доставки
+            title=title,
+            body=body,
+            parse_mode=parse_mode,
+            render_kind="notice",    # на всякий случай дублируем
+            force_new=True,
+            persist_key=persist_key or f"notify:{user_tg_id}",
+            disable_web_page_preview=True,
+        )
+    except Exception as ex:
+        logging.error(f"Error during notify: {ex}")
